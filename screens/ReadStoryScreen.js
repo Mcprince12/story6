a3d966edc0458508afbd9a8ff2814b22fa23b39f
Mcprince12 <sohumtrip@gmail.com>
2021-02-04 14:31:59 +0200

    Flatlist not working

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,27 +1,93 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import {SearchBar} from 'react-native-elements';
+import {ScrollView} from 'react-native-gesture-handler'
+import db from '../config'
 
 export default class ReadStoryScreen extends React.Component{
+   constructor(){
+       super();
+       this.state={
+           search:'',
+           allInputs:[],
+           lastVisibleSubmission:null,
+       };
+   }
+
+   updateSearch=(search)=>{
+       this.setState({search});
+   }
+
+   retrieveStory = async() =>{
+        var text = this.state.search;
+        var enteredText = text.split("");
+        if(enteredText[0].toUpperCase()==='A'){
+            const query = await db.collection("Story").where('Story', '==', text)
+            .startAfter(this.state.lastVisibleSubmission).limit(10).get();
+            query.docs.map((doc)=>{
+                this.setState({
+                    allInputs:[...this.state.allInputs, doc.data()],
+                    lastVisibleSubmission:doc,
+                })
+            })
+        } else if(enteredText[0].toUpperCase()==='A'){
+            const query = await db.collection("Author").where('AuthorName', '==', text)
+            .startAfter(this.state.lastVisibleSubmission).limit(10).get();
+            query.docs.map((doc)=>{
+                this.setState({
+                    allInputs:[...this.state.allInputs, doc.data()],
+                    lastVisibleSubmission:doc,
+                })
+            })      
+         }
+   }
+
+   
+
     render(){
         return(
             <View style = {styles.container}>
-                <Text style = {styles.text}>
-                    Read Story
-                </Text>
+                <SearchBar
+                    placeholder="Type Here..."
+                    onChangeText={this.updateSearch}
+                    value={this.state.search}
+                    style = {styles.searchBar}
+                />
+                <ScrollView>
+                <FlatList
+                    style = {styles.bar}
+                    data = {this.state.allInputs}
+                    renderItem={({item})=>{
+                        <View style = {{borderBottomWidth:2}}>
+                            <Text>
+                                {"Title "+item.text1}
+                            </Text>
+                            <Text>
+                                {"AuthorName "+item.text}
+                            </Text>
+                            <Text>
+                                {"Story "+item.text2}
+                            </Text>
+
+                        </View>
+                    }}
+                    keyExtractor={(item, index)=>{
+                        index.toString()
+                    }}
+                    onEndReached={this.retrieveStory}
+                    onEndReachedThreshold={0.7}
+                />
+                </ScrollView>
             </View>
         )
     }
 }
 
-const styles = StyleSheet.create({
-    text:{
-        fontSize:40,
-        fontFamily:'Times New Roman',
-        textAlign:'center'
-    },
+const styles = StyleSheet.create({    
+    container: {      flex: 1,     
+         marginTop: 20    }, 
 
-    container:{
-        alignSelf:'center',
-        justifyContent:'center'
-    }
-})
+     searchBar:{      flexDirection:'row',      height:40,      width:'auto',      borderWidth:0.5,      alignItems:'center',      backgroundColor:'grey',      },   
+    bar:{      borderWidth:2,      height:30,      width:300,      paddingLeft:10,    },    
+    searchButton:{      borderWidth:1,      height:30,      width:50,      alignItems:'center',      justifyContent:'center',      backgroundColor:'green'    } 
+ })
